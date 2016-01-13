e82b739571587b0de9b688188390e219b4bd00ef
karcio <kczarnecki@hotmail.com>
2016-01-13 21:44:36 +0000

    added GUI

---
+++ bin/design.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'tariffCalculatorGUI.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(240, 320)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 10, 111, 20))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(10, 40, 121, 16))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(80, 250, 75, 24))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.lineEdit = QtGui.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(152, 10, 81, 20))
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.lineEdit_2 = QtGui.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(152, 40, 81, 20))
+        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
+        self.lineEdit_3 = QtGui.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setGeometry(QtCore.QRect(60, 130, 113, 20))
+        self.lineEdit_3.setObjectName(_fromUtf8("lineEdit_3"))
+        self.label_3 = QtGui.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(40, 100, 161, 16))
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.label_4 = QtGui.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(10, 190, 91, 16))
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.label_5 = QtGui.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(10, 220, 91, 16))
+        self.label_5.setObjectName(_fromUtf8("label_5"))
+        self.label_6 = QtGui.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(120, 190, 57, 14))
+        self.label_6.setObjectName(_fromUtf8("label_6"))
+        self.label_7 = QtGui.QLabel(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(120, 220, 57, 14))
+        self.label_7.setObjectName(_fromUtf8("label_7"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 240, 19))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.label.setText(_translate("MainWindow", "day tariff in cents", None))
+        self.label_2.setText(_translate("MainWindow", "night tariff in cents", None))
+        self.pushButton.setText(_translate("MainWindow", "calculate", None))
+        self.label_3.setText(_translate("MainWindow", "counter reading per hour", None))
+        self.label_4.setText(_translate("MainWindow", "Monthly costs", None))
+        self.label_5.setText(_translate("MainWindow", "Yearly costs", None))
+        self.label_6.setText(_translate("MainWindow", "TextLabel", None))
+        self.label_7.setText(_translate("MainWindow", "TextLabel", None))
+
---
+++ bin/tariffCalclulatorGUI.py
@@ -0,0 +1,47 @@
+'''
+Created on Jan 13, 2016
+simple day/night tariff calculator
+@author: karcio
+'''
+import sys
+
+from PyQt4 import QtGui
+
+import design
+
+
+class ExampleApp(QtGui.QMainWindow, design.Ui_MainWindow):
+    def __init__(self, parent=None):
+        
+        super(ExampleApp, self).__init__(parent)
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.calculate) 
+
+    
+    def calculate(self):
+        dayTariffCost = self.lineEdit.text()
+        D_TARIFF_COST = float(dayTariffCost)
+        
+        nightTariffCost = self.lineEdit_2.text()
+        N_TARIFF_COST = float(nightTariffCost)
+        
+        counterReading = self.lineEdit_3.text()
+        cr = float(counterReading)
+        
+        day = cr * 0.625 * D_TARIFF_COST
+        night = cr * 0.375 * N_TARIFF_COST
+        
+        calculation = day + night 
+        
+        self.label_6.setText(repr(calculation * 30))
+        self.label_7.setText(repr(calculation * 360))
+            
+               
+def main():
+    app = QtGui.QApplication(sys.argv)  
+    form = ExampleApp()                 
+    form.show()                         
+    app.exec_()                        
+
+if __name__ == '__main__':              
+    main()
